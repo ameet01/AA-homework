8459dd2c279148a5f34ecff5ce69739813258623
Ameet Vadhia <ameet01@gmail.com>
2017-10-05 21:52:57 -0700

    implemented Map DS

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -38,5 +38,42 @@ end
 
 
 class Map
+  def initialize
+    @map = Array.new
+  end
 
+  def assign(key, value)
+    includes = false
+
+    @map.each do |i|
+      if i[0] == key
+        i[1] = value
+        includes = true
+        break
+      end
+    end
+
+    if !includes
+      @map << [key, value]
+    end
+
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.each do |array|
+      if array[0] == key
+        return array[1]
+      end
+    end
+    nil
+  end
+
+  def remove(key)
+    @map = @map.select! {|i| i[0] != key}
+  end
+
+  def show
+    @map.dup
+  end
 end
