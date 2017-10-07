244e40555281a70f0f1d6205c93f92a08169d73a
Ameet Vadhia <ameet01@gmail.com>
2017-10-07 15:47:53 -0700

    Updated main functionality

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -10,19 +10,44 @@ class Simon
   end
 
   def play
-
+    until @game_over == true
+      take_turn
+    end
+    if @game_over
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if !game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    p @seq
   end
 
   def require_sequence
+    hash = {
+      'red' => 'r',
+      'blue' => 'b',
+      'green' => 'g',
+      'yellow' => 'y'
+    }
+    puts 'Please enter sequence (rbgy):'
+    guess = gets.chomp
 
+    guess.split('').each_with_index do |letter, index|
+      if letter != hash[@seq[index]] || (guess.length != @seq.length)
+        @game_over = true
+      end
+    end
   end
 
   def add_random_color
@@ -30,14 +55,19 @@ class Simon
   end
 
   def round_success_message
-
+    p 'Correct!'
   end
 
   def game_over_message
-
+    p 'The game is over'
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = Array.new
   end
 end
+
+game = Simon.new
+game.play
