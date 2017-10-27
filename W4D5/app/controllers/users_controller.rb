5476fb0aa978230c9839b2472d12dafe148e1228
Ameet Vadhia <ameet01@gmail.com>
2017-10-26 21:13:09 -0700

    implement mail and helpers

--- W4D5/app/controllers/users_controller.rb
+++ W4D5/app/controllers/users_controller.rb
@@ -4,8 +4,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      login_user!(@user)
-      redirect_to cats_url
+       login_user!(@user)
+       msg = UserMailer.welcome_email(@user)
+       msg.deliver_now
+       redirect_to cats_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:password, :username)
   end
