7f5c425ce950457ad5d3b601f2142f84f6653a63
Ameet Vadhia <ameet01@gmail.com>
2017-11-01 22:46:59 -0700

    add semicolons

--- W5D4/introproblems2.js
+++ W5D4/introproblems2.js
@@ -1,14 +1,14 @@
 function titleize(array, callback) {
   let result = array.map(function(i) {
     return "Mx. " + (i[0].toUpperCase() + i.slice(1)) + " Jingleheimer Schmidt";
-  })
+  });
   callback(result);
 }
 
 titleize(['hello','there','good','sir'], function(names) {
   names.forEach(function(name) {
     console.log(name);
-  })
+  });
 });
 
 
@@ -16,38 +16,38 @@ let Elephant = function(name, height, tricks) {
   this.name = name;
   this.height = height;
   this.tricks = tricks;
-}
+};
 
 Elephant.prototype.trumpet = function() {
   console.log(`${this.name} the elephant goes phrRRRRRRRRR!!!!!!`);
-}
+};
 
 Elephant.prototype.grow = function() {
   this.height += 12;
-}
+};
 
 Elephant.prototype.addTrick = function(trick) {
-  this.tricks.push(trick)
-}
+  this.tricks.push(trick);
+};
 
 Elephant.prototype.play = function() {
   rand = Math.floor(Math.random() * this.tricks.length);
-  console.log(this.tricks[rand])
-}
+  console.log(this.tricks[rand]);
+};
 
 Elephant.paradeHelper = function(elephant) {
-  console.log(`${elephant.name} is trotting by!`)
-}
+  console.log(`${elephant.name} is trotting by!`);
+};
 
 
 
 function dinerBreakfast() {
   let order = "I'd like cheesy scrambled eggs please";
-  console.log(order)
+  console.log(order);
 
   return function(food) {
-    order += ' '
-    order += food
-    console.log(order)
-  }
+    order += ' ';
+    order += food;
+    console.log(order);
+  };
 }
