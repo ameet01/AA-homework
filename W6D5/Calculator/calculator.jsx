5aab66174b5dabb3dcb05b44b17226303f952f87
Ameet Vadhia <ameet01@gmail.com>
2017-11-10 00:18:01 -0800

    finish first component

--- W6D5/Calculator/calculator.jsx
+++ W6D5/Calculator/calculator.jsx
@@ -6,22 +6,67 @@ class Calculator extends React.Component {
     this.state = {result: 0, num1: "", num2: ""};
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
   setNum1(event) {
-    event.preventDefault();
+    const num1 = event.target.value ? parseInt(event.target.value) : "";
+    this.setState({ num1 });
   }
 
   setNum2(event) {
+    const num2 = event.target.value ? parseInt(event.target.value) : "";
+    this.setState({ num2 });
+  }
+
+  add(event) {
     event.preventDefault();
+    var result = this.state.num1 + this.state.num2;
+    this.setState({result});
+  }
+
+  subtract(event) {
+    event.preventDefault();
+    var result = this.state.num1 - this.state.num2;
+    this.setState({result});
+  }
+
+  divide(event) {
+    event.preventDefault();
+    var result = Math.floor(this.state.num1 / this.state.num2);
+    this.setState({result});
+  }
+
+  multiply(event) {
+    event.preventDefault();
+    var result = this.state.num1 * this.state.num2;
+    this.setState({result});
+  }
+
+  clear(event) {
+    event.preventDefault();
+    this.setState({result:0,num1:"",num2:""});
   }
 
   render() {
+    const {result, num1, num2} = this.state;
+
     return (
       <div>
-        <h1>{this.state.result}</h1>
-        <input onChange={this.setNum1} value={this.state.num1}></input>
-        <input onChange={this.setNum2} value={this.state.num2}></input>
+        <h1>{result}</h1>
+        <input value={num1} onChange={this.setNum1}></input>
+        <input value={num2} onChange={this.setNum2}></input>
+        <button onClick={this.clear}>Clear</button>
+
+        <br />
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
       </div>
     );
   }
